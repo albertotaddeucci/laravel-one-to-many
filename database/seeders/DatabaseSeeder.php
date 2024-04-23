705a695d7dc8fbf02218adc0eca545781b8f9a26
alberto <alberto.taddeucci@gmail.com>
2024-04-23 15:24:01 +0200

    feat: creazione tabella types e seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +17,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ProjectSeeder::class,
+            TypeSeeder::class,
         ]);
     }
 }
